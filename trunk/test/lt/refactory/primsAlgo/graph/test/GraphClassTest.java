09daeb55ae67c52380826b1344831cafa63d37b9
Sadauskas.Egidijus@gmail.com <Sadauskas.Egidijus@gmail.com@39896449-9050-c8bc-998c-5d036d38b006>
2012-10-03 13:30:31 +0000

    Testing Adding to graph capabilities . Got NullPointerException in at Graph.java class line 32.

--- trunk/test/lt/refactory/primsAlgo/graph/test/GraphClassTest.java
+++ trunk/test/lt/refactory/primsAlgo/graph/test/GraphClassTest.java
@@ -5,6 +5,13 @@ package lt.refactory.primsAlgo.graph.test;
 
 import static org.junit.Assert.*;
 
+import java.math.BigDecimal;
+
+import lt.refactory.primsAlgo.graph.Edge;
+import lt.refactory.primsAlgo.graph.Graph;
+import lt.refactory.primsAlgo.graph.Node;
+import lt.refactory.primsAlgo.graph.exception.AddNodeException;
+
 import org.junit.Test;
 
 /**
@@ -15,10 +22,24 @@ public class GraphClassTest {
 
 	/**
 	 * Test method for {@link lt.refactory.primsAlgo.graph.Graph#addNode(lt.refactory.primsAlgo.graph.Node)}.
+	 * @throws AddNodeException 
 	 */
 	@Test
-	public void testAddNode() {
-		fail("Not yet implemented");
+	public void testAddNode() throws AddNodeException {
+		BigDecimal bigNumber1 = new BigDecimal(1.0);
+		BigDecimal bigNumber2 = new BigDecimal(2.0);
+		BigDecimal bigNumber3 = new BigDecimal(3.0);
+		Graph<Edge> graph = new Graph<Edge>();
+		
+		// creating point 
+		Node point_1 = new Node(bigNumber1, bigNumber2);
+		
+		// adding point to graph
+		graph.addNode(point_1);
+		
+		assertTrue(graph.containsNode(point_1));
+		
+		
 	}
 
 	/**
