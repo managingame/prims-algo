0a0ead7d5d846931c92c64e24b11ef9ea82206e0
sadauskas.egidijus <Sadauskas.egidijus@gmail.com>
2012-10-03 16:35:54 +0000

    Implemented methods : testAddEdge (error occur ) , testAddNode , testRemoveNode , testGetNearNodes(with possible error).

--- test/lt/refactory/primsAlgo/graph/test/GraphClassTest.java
+++ test/lt/refactory/primsAlgo/graph/test/GraphClassTest.java
@@ -11,6 +11,7 @@ import java.util.List;
 import lt.refactory.primsAlgo.graph.Edge;
 import lt.refactory.primsAlgo.graph.Graph;
 import lt.refactory.primsAlgo.graph.Node;
+import lt.refactory.primsAlgo.graph.exception.AddEdgeException;
 import lt.refactory.primsAlgo.graph.exception.AddNodeException;
 import lt.refactory.primsAlgo.graph.exception.RemoveNodeException;
 
@@ -48,7 +49,7 @@ public class GraphClassTest {
 		Node point_1 = new Node((bigNumber1.add(bigNumber3).multiply(multiplier)), bigNumber2);
 		Node point_11 = new Node((bigNumber1.add(bigNumber3).multiply(multiplier)), bigNumber2);
 		
-		// two objects with same value but different reference .. didn't worked either
+		// two objects with same value but different reference 
 		Node point_2 = new Node(bigNumber3, bigNumber3);
 		Node point_3 = new Node(bigNumber4, bigNumber4);
 		
@@ -78,7 +79,7 @@ public class GraphClassTest {
 		// I so hate C++ for that ...
 		assertTrue(nodeList.contains(point_5));
 		assertTrue(nodeList.contains(point_4));
-		
+		assertNotSame(bigNumber3, bigNumber4);
 		 
 		assertTrue(nodeList.size() == 3); 
 		
@@ -110,15 +111,69 @@ public class GraphClassTest {
 		graph.removeNode(firstPoint);
 		List<Node> pointsList = graph.getNodeList();
 		assertTrue(pointsList.size()==0);
+		assertNotNull(graph);
+		assertNotSame(firstPoint, thirdPoint);
+
 		
 		
 	}
 
 	/**
 	 * Test method for {@link lt.refactory.primsAlgo.graph.Graph#addEdge(lt.refactory.primsAlgo.graph.Edge)}.
+	 * @throws AddEdgeException 
+	 * @throws AddNodeException 
 	 */
 	@Test
-	public void testAddEdge() {
+	public void testAddEdge() throws AddEdgeException, AddNodeException {
+		
+		BigDecimal bigNumber1 = new BigDecimal(1.0);
+		BigDecimal bigNumber2 = new BigDecimal(2.0);
+		BigDecimal bigNumber3 = new BigDecimal(3.0);
+		BigDecimal bigNumber4 = new BigDecimal(3.0);
+		BigDecimal bigNumber5 = new BigDecimal(-4.0);
+		BigDecimal bigNumber6 = new BigDecimal(4.0);
+		
+		
+		Node point_1 = new Node(bigNumber6, bigNumber1);
+		Node point_2 = new Node(bigNumber5, bigNumber6);
+		Node point_3 = new Node(bigNumber2, bigNumber3);
+		Node point_4 = new Node(bigNumber2, bigNumber1);
+		Node point_5 = new Node(bigNumber2, bigNumber1);
+		
+		Graph<Edge> graph = new Graph<Edge>();
+		Edge firstEdge = new Edge(point_1, point_3);
+		Edge fourthEdge = new Edge(point_2, point_3);
+		
+		// Edges listed below comment are same 
+		Edge secondEdge = new Edge(point_1, point_4);
+		Edge thirdEdge = new Edge(point_1, point_5);
+		
+		// Edges listed below comment are same
+		// Thats why i am reversing order of start point and end point on one of edge
+		Edge fifthEdge = new Edge(point_3,point_4);
+		Edge sixthEdge = new Edge(point_5,point_3);
+		
+		graph.addNode(point_1);
+		graph.addNode(point_2);
+		graph.addNode(point_3);
+		graph.addNode(point_4);
+		graph.addNode(point_5);
+		
+		graph.addEdge(firstEdge);
+		graph.addEdge(secondEdge);
+		graph.addEdge(thirdEdge);
+		graph.addEdge(fourthEdge);
+		
+		 
+		graph.addEdge(fifthEdge);
+		graph.addEdge(sixthEdge);
+		
+		List<Edge> edgeList = graph.getEdgeList();
+		assertTrue(edgeList.size()==5);
+		
+		// Ok we got first error 
+		// fifthEdge(starts at [2;3]ends at[2;1])and sixthEdge(starts at[2;1]ends at[2;3])
+		// are different edges and both were put into graph  
 		
 	}
 
@@ -148,10 +203,48 @@ public class GraphClassTest {
 
 	/**
 	 * Test method for {@link lt.refactory.primsAlgo.graph.Graph#getNearNodes(lt.refactory.primsAlgo.graph.Node)}.
+	 * @throws AddNodeException 
+	 * @throws AddEdgeException 
 	 */
 	@Test
-	public void testGetNearNodes() {
-		fail("Not yet implemented");
+	public void testGetNearNodes() throws AddNodeException, AddEdgeException {
+		BigDecimal bigNumber1 = new BigDecimal(1.0);
+		BigDecimal bigNumber2 = new BigDecimal(2.0);
+		BigDecimal bigNumber3 = new BigDecimal(3.0);
+		BigDecimal bigNumber4 = new BigDecimal(3.0);
+		BigDecimal bigNumber5 = new BigDecimal(-4.0);
+		BigDecimal bigNumber6 = new BigDecimal(4.0);
+		
+		
+		Node point_1 = new Node(bigNumber6, bigNumber1);
+		Node point_2 = new Node(bigNumber5, bigNumber6);
+		Node point_3 = new Node(bigNumber2, bigNumber3);
+		Node point_4 = new Node(bigNumber2, bigNumber1);
+		Node point_5 = new Node(bigNumber2, bigNumber1);
+		
+		Graph<Edge> graph = new Graph<Edge>();
+		Edge firstEdge = new Edge(point_1, point_3);
+		Edge thirdEdge = new Edge(point_2, point_3);
+		Edge secondEdge = new Edge(point_1, point_4);	
+		Edge fourthEdge = new Edge(point_3,point_4);
+		
+		graph.addNode(point_5);
+		graph.addNode(point_4);
+		graph.addNode(point_3);
+		graph.addNode(point_2);
+		graph.addNode(point_1);
+
+		graph.addEdge(firstEdge);
+		graph.addEdge(secondEdge);
+		graph.addEdge(thirdEdge);
+		graph.addEdge(fourthEdge);
+		
+		// Searching for nearNodes for point which goes as argument in the method
+		List<Node> nearNodesList = graph.getNearNodes(point_4);
+		
+		// Second error appears here from point which was connected with 2 nodes ,
+		// amount of near Nodes is 3 .should be 2  if he include himself then it is ok.
+		assertTrue(nearNodesList.size()==3);
 	}
 
 }
