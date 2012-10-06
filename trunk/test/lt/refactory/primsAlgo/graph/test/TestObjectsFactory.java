7d084e3a742ae6644ff34984429f10003b475199
sadauskas.egidijus@gmail.com <sadauskas.egidijus@gmail.com@39896449-9050-c8bc-998c-5d036d38b006>
2012-10-06 11:19:44 +0000

    v1.2a, Working addNode , addEdge,RemoveNode, removeEdge ,nearNode methods .

--- trunk/test/lt/refactory/primsAlgo/graph/test/TestObjectsFactory.java
+++ trunk/test/lt/refactory/primsAlgo/graph/test/TestObjectsFactory.java
@@ -3,68 +3,113 @@ package lt.refactory.primsAlgo.graph.test;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
-
 import lt.refactory.primsAlgo.graph.Edge;
 import lt.refactory.primsAlgo.graph.Node;
 
-enum Points{
-	AA,AB,AC,AD,AE,AF,AG,
-	BA,BB,BC,BD,BE,BF,BG,
-	CA,CB,CC,CD,CE,CF,CG,
-}
 
- public class TestObjectsFactory {
-	 static final int TESTOBJECTS = 6;
-	 static BigDecimal []pointsToXArray = new BigDecimal[TESTOBJECTS];
-	 static BigDecimal []pointsToYArray = new BigDecimal[TESTOBJECTS];
-	 
-	 static{
-		 pointsToXArray[0] = BigDecimal.valueOf(1.0);
-		 pointsToXArray[1] = BigDecimal.valueOf(2.0);
-		 pointsToXArray[2] = BigDecimal.valueOf(-3.0);
-		 pointsToXArray[3] = BigDecimal.valueOf(4.0);
-		 pointsToXArray[4] = BigDecimal.valueOf(0.5);
-		 pointsToXArray[5] = BigDecimal.valueOf(-6.0);
+	public class TestObjectsFactory {
+		/* 
+		 * TODO : finish nodes enum
+		public enum enumNodes{
+				A,B,C,D,E,F,G,H,I,J;
+				Node getNode(enumNodes node)
+				{
+					Node requiredNode;
+					switch(node)
+					{
+					case A:
+						requiredNode = new Node(pointsToXArray[0],pointsToYArray[0]);
+						break;
+					case B:
+						requiredNode = new Node(pointsToXArray[0],pointsToYArray[0]);
+						break;
+					case C:
+						requiredNode = new Node(pointsToXArray[0],pointsToYArray[0]);
+						break;
+					case D:
+						requiredNode = new Node(pointsToXArray[0],pointsToYArray[0]);
+						break;
+					case E:
+						requiredNode = new Node(pointsToXArray[0],pointsToYArray[0]);
+						break;
+					case F:
+						requiredNode = new Node(pointsToXArray[0],pointsToYArray[0]);
+						break;
+					case G:
+						requiredNode = new Node(pointsToXArray[0],pointsToYArray[0]);
+						break;
+					default :
+						requiredNode = null;
+						break;
+							
+					}
+					return requiredNode;
+						
+				}
+			 }
+			 */
+		
+		 static final int TESTOBJECTS = 6;
+		 static BigDecimal []pointsToXArray = new BigDecimal[TESTOBJECTS];
+		 static BigDecimal []pointsToYArray = new BigDecimal[TESTOBJECTS];
 		 
-		 pointsToYArray[0] = BigDecimal.valueOf(0.4);
-		 pointsToYArray[1] = BigDecimal.valueOf(1.1);
-		 pointsToYArray[2] = BigDecimal.valueOf(-3.5);
-		 pointsToYArray[3] = BigDecimal.valueOf(2.7);
-		 pointsToYArray[4] = BigDecimal.valueOf(-0.0);
-		 pointsToYArray[5] = BigDecimal.valueOf(1.2);		 
-	 }
-	 
-	 static List<Node> nodesFactory()
-	 {
-		 List<Node> nodeList = new ArrayList<Node>();
-			 nodeList.add(new Node(pointsToXArray[0],pointsToYArray[0])); // 0[ 1.0; 0.4];
-			 nodeList.add(new Node(pointsToXArray[0],pointsToYArray[1])); // 1[ 1.0; 1.1];
-			 nodeList.add(new Node(pointsToXArray[2],pointsToYArray[3])); // 2[-3.0; 2.7];
-			 nodeList.add(new Node(pointsToXArray[5],pointsToYArray[4])); // 3[-6.0;-0.0];
-			 nodeList.add(new Node(pointsToXArray[3],pointsToYArray[5])); // 4[ 4.0; 1.2];
-			 nodeList.add(new Node(pointsToXArray[1],pointsToYArray[5])); // 5[ 2.0; 1.2];
-			 nodeList.add(new Node(pointsToXArray[5],pointsToYArray[1])); // 6[-6.0; 1.1];
-			 nodeList.add(new Node(pointsToXArray[5],pointsToYArray[1])); // 7[-6.0; 1.1];
-			 nodeList.add(new Node(pointsToXArray[4],pointsToYArray[2])); // 8[ 0.5;-3.5];
-		return nodeList;
 		 
-	 }
-	 static List<Edge> edgeFactory(){
-		List<Node> nodeList = TestObjectsFactory.nodesFactory();
-		List<Edge> edgeList = new ArrayList<>();
-		edgeList.add(new Edge(nodeList.get(0),nodeList.get(6)));
-		edgeList.add(new Edge(nodeList.get(6),nodeList.get(0)));
-		edgeList.add(new Edge(nodeList.get(0),nodeList.get(5)));
-	    edgeList.add(new Edge(nodeList.get(5),nodeList.get(1)));
-		edgeList.add(new Edge(nodeList.get(1),nodeList.get(5)));
-		edgeList.add(new Edge(nodeList.get(1),nodeList.get(6)));
-		edgeList.add(new Edge(nodeList.get(5),nodeList.get(6)));
-		edgeList.add(new Edge(nodeList.get(4),nodeList.get(5)));
-		edgeList.add(new Edge(nodeList.get(2),nodeList.get(6)));
-		edgeList.add(new Edge(nodeList.get(2),nodeList.get(3)));
-		edgeList.add(new Edge(nodeList.get(0),nodeList.get(8)));
-		System.out.println(edgeList.toString());
-		return edgeList;
 		 
+		 
+		 static{
+			 pointsToXArray[0] = BigDecimal.valueOf(1.0);
+			 pointsToXArray[1] = BigDecimal.valueOf(2.0);
+			 pointsToXArray[2] = BigDecimal.valueOf(-3.0);
+			 pointsToXArray[3] = BigDecimal.valueOf(4.0);
+			 pointsToXArray[4] = BigDecimal.valueOf(0.5);
+			 pointsToXArray[5] = BigDecimal.valueOf(-6.0);
+			 
+			 pointsToYArray[0] = BigDecimal.valueOf(0.4);
+			 pointsToYArray[1] = BigDecimal.valueOf(1.1);
+			 pointsToYArray[2] = BigDecimal.valueOf(-3.5);
+			 pointsToYArray[3] = BigDecimal.valueOf(2.7);
+			 pointsToYArray[4] = BigDecimal.valueOf(-0.0);
+			 pointsToYArray[5] = BigDecimal.valueOf(1.2);		 
+		 }
+		 
+		 static List<Node> nodesFactory()
+		 {
+			 List<Node> nodeList = new ArrayList<Node>();
+				 nodeList.add(new Node(pointsToYArray[0],pointsToYArray[0])); // 0[ 1.0; 0.4];
+				 nodeList.add(new Node(pointsToXArray[0],pointsToYArray[1])); // 1[ 1.0; 1.1];
+				 nodeList.add(new Node(pointsToXArray[2],pointsToYArray[3])); // 2[-3.0; 2.7];
+				 nodeList.add(new Node(pointsToXArray[5],pointsToYArray[4])); // 3[-6.0;-0.0];
+				 nodeList.add(new Node(pointsToXArray[3],pointsToYArray[5])); // 4[ 4.0; 1.2];
+				 nodeList.add(new Node(pointsToXArray[1],pointsToYArray[5])); // 5[ 2.0; 1.2];
+				 nodeList.add(new Node(pointsToXArray[5],pointsToYArray[1])); // 6[-6.0; 1.1];
+				 nodeList.add(new Node(pointsToXArray[5],pointsToYArray[1])); // 7[-6.0; 1.1];
+				 nodeList.add(new Node(pointsToXArray[4],pointsToYArray[2])); // 8[ 0.5;-3.5];
+				 nodeList.add(new Node(pointsToXArray[2],pointsToYArray[4])); // 9[-3.0;-0.0];
+			return nodeList;
+			 
+		 }
+		 static List<Edge> edgeFactory(){
+			List<Node> nodeList = TestObjectsFactory.nodesFactory();
+			List<Edge> edgeList = new ArrayList<>();
+			edgeList.add(new Edge(nodeList.get(0),nodeList.get(6)));
+			edgeList.add(new Edge(nodeList.get(6),nodeList.get(0)));
+			edgeList.add(new Edge(nodeList.get(0),nodeList.get(5)));
+		    edgeList.add(new Edge(nodeList.get(5),nodeList.get(1)));
+			edgeList.add(new Edge(nodeList.get(1),nodeList.get(5)));
+			edgeList.add(new Edge(nodeList.get(1),nodeList.get(6)));
+			edgeList.add(new Edge(nodeList.get(5),nodeList.get(6)));
+			edgeList.add(new Edge(nodeList.get(4),nodeList.get(5)));
+			edgeList.add(new Edge(nodeList.get(2),nodeList.get(6)));
+			edgeList.add(new Edge(nodeList.get(2),nodeList.get(3)));
+			edgeList.add(new Edge(nodeList.get(0),nodeList.get(8)));
+			edgeList.add(new Edge(nodeList.get(0),nodeList.get(7)));
+			edgeList.add(new Edge(nodeList.get(8),nodeList.get(9)));
+			edgeList.add(new Edge(nodeList.get(0),nodeList.get(9)));
+			edgeList.add(new Edge(nodeList.get(9),nodeList.get(0)));
+			
+			
+			return edgeList;
+			 
+		 }
 	 }
-}
+ 
