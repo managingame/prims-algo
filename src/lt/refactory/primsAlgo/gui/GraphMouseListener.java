13bb5a254da4a4d510d762ff0a8e376a26616dfc
sadauskas.egidijus <Sadauskas.egidijus@gmail.com>
2012-10-12 14:27:59 +0000

    V0.5 , some updated methods . Now it is painting nodes and edges and it wont paint another nodes on old if node already exists on certain coordinate

--- src/lt/refactory/primsAlgo/gui/GraphMouseListener.java
+++ src/lt/refactory/primsAlgo/gui/GraphMouseListener.java
@@ -6,7 +6,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GraphMouseListener implements MouseListener {
-
+	private List<ArrayList<Integer>> pointList ;
+	private List<ArrayList<Integer>> edgeList;
+	private List<Integer> edge;
+	private List<Integer> point;
+	private List<Integer> point2;
+	
 	int x;
 	int y;
 	GraphPanel panel;
@@ -22,15 +27,32 @@ public class GraphMouseListener implements MouseListener {
 	{
 		
 		this.panel = panel;
+		pointList = new ArrayList<>();
+		edgeList = new ArrayList<>();
 	}
 	
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		endedwhen = e.getWhen();
-		endedWhereX = e.getXOnScreen();
-		endedWhereY = e.getYOnScreen();
+		endedWhereX = e.getXOnScreen()-3;
+		endedWhereY = e.getYOnScreen()-3;
+		point = new ArrayList<>();
+		point.add(startedWhereX);
+		point.add(startedWhereY);
+		point2 = new ArrayList<>();
+		point2.add(endedWhereX);
+		point2.add(endedWhereY);
+		
 		if (((endedwhen-startedwhen)%600)>200 ||((endedwhen-startedwhen)/600) > 0 )
 		{
+			if (!pointList.contains(point))
+			{
+				panel.AddPoint(point);
+			}
+			if (!pointList.contains(point2))
+			{
+				panel.AddPoint(point2);
+			}
 			
 			panel.AddEdge(startedWhereX, startedWhereY, endedWhereX, endedWhereY);
 		}
@@ -39,8 +61,8 @@ public class GraphMouseListener implements MouseListener {
 	@Override
 	public void mousePressed(MouseEvent e) {
 		startedwhen = e.getWhen();
-		startedWhereX = e.getXOnScreen();
-		startedWhereY = e.getYOnScreen();
+		startedWhereX = e.getXOnScreen()-3;
+		startedWhereY = e.getYOnScreen()-3;
 		
 	}
 	
@@ -63,6 +85,7 @@ public class GraphMouseListener implements MouseListener {
 		List<Integer> point = new ArrayList<>();
 		point.add(x);
 		point.add(y);
+		pointList.add((ArrayList<Integer>) point);
 		panel.AddPoint(x, y);
 		
 		
