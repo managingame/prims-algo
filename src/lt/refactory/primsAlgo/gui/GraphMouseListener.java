10490b82b3142caba4f9bb48ecd7d995effca4c8
sadauskas.egidijus <Sadauskas.egidijus@gmail.com>
2012-11-03 14:32:16 +0000

    Latest version of GUI . Full graph construction using fullGraphFactory

--- src/lt/refactory/primsAlgo/gui/GraphMouseListener.java
+++ src/lt/refactory/primsAlgo/gui/GraphMouseListener.java
@@ -1,100 +1,35 @@
 package lt.refactory.primsAlgo.gui;
 
+
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.util.ArrayList;
-import java.util.List;
+import java.math.BigDecimal;
+
+import lt.refactory.primsAlgo.graph.Edge;
+import lt.refactory.primsAlgo.graph.Graph;
+import lt.refactory.primsAlgo.graph.Node;
+import lt.refactory.primsAlgo.graph.exception.AddNodeException;
 
 public class GraphMouseListener implements MouseListener {
-	private List<ArrayList<Integer>> pointList ;
-	private List<ArrayList<Integer>> edgeList;
-	private List<Integer> edge;
-	private List<Integer> point;
-	private List<Integer> point2;
-	public final static int TVORA = 5;
+	private Graph<Edge> graph ;
 	
-	int x;
-	int y;
+	
+	public final static int GAP = 15;
+	
+	BigDecimal x;
+	BigDecimal y;
 	GraphPanel panel;
-	private long startedwhen;
-	private int startedWhereX;
-	private int startedWhereY;
-	
-	private long endedwhen;
-	private int endedWhereX;
-	private int  endedWhereY;
-	
 	public GraphMouseListener(GraphPanel panel)
 	{
 		
 		this.panel = panel;
-		pointList = new ArrayList<>();
-		edgeList = new ArrayList<>();
+		graph = new Graph<>();
 		
 	}
 	
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		System.out.println("Realise");
-		endedwhen = e.getWhen();
-		endedWhereX = e.getXOnScreen()-3-panel.getLocationOnScreen().x;
-		endedWhereY = e.getYOnScreen()-3-panel.getLocationOnScreen().y;
-		point = new ArrayList<>();
-		point2 = new ArrayList<>();
-		point.add(startedWhereX);
-		point.add(startedWhereY);
-		
-		
-		if (((endedwhen-startedwhen)%600)>200 ||((endedwhen-startedwhen)/600) > 0 )
-		{
-		
-			for (ArrayList<Integer> point : pointList) {
-				System.out.println("cia turetu kazka spausdinti"+point.toString());
-				if (isNear(point,endedWhereX,endedWhereY)){
-					endedWhereX = point.get(0);
-					endedWhereY = point.get(1);
-					break;
-				}
-			}
-			
-			
-			point2.add(endedWhereX);
-			point2.add(endedWhereY);
-			panel.AddPoint(point);
-			panel.AddPoint(point2);
-			panel.AddEdge(startedWhereX, startedWhereY, endedWhereX, endedWhereY);
-		}
-	}
 	
-	@Override
-	public void mousePressed(MouseEvent e) {
-		System.out.println("Kviestas press");
-		startedwhen = e.getWhen();
-		startedWhereX = e.getXOnScreen()-3-panel.getLocationOnScreen().x;
-		startedWhereY = e.getYOnScreen()-3-panel.getLocationOnScreen().y;
-		for (ArrayList<Integer> point : pointList) {
-			if (isNear(point,startedWhereX,startedWhereY))
-			{
-				startedWhereX = point.get(0);
-				startedWhereY = point.get(1);
-				break;
-			}
-		}
 		
-	}
 	
-	private boolean isNear(ArrayList<Integer> point3, int startedWhereX2,
-			int startedWhereY2) {
-		int diferenceXAxis = Math.abs(point3.get(0)-startedWhereX2);
-		int diferenceYAxis = Math.abs(point3.get(1)-startedWhereY2);
-		System.out.println("Skirtumas:"+diferenceXAxis+"x asi"+diferenceYAxis);
-			if (diferenceXAxis <=5+TVORA && diferenceYAxis <=5+TVORA){
-				return true;
-			}else{
-				return false;
-			}
-			
-	}
 
 	@Override
 	public void mouseExited(MouseEvent e) {
@@ -110,28 +45,79 @@ public class GraphMouseListener implements MouseListener {
 	
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		System.out.println("Kviestas click");
-		x = e.getXOnScreen()-3-panel.getLocationOnScreen().x;
-		y = e.getYOnScreen()-3-panel.getLocationOnScreen().y;
-		List<Integer> point = new ArrayList<>();
 		
-		for (ArrayList<Integer> testPoint : pointList) {
-			if(isNear(testPoint, x, y))
+		
+		x = BigDecimal.valueOf(e.getXOnScreen()-3-panel.getLocationOnScreen().x);
+		y = BigDecimal.valueOf(e.getYOnScreen()-3-panel.getLocationOnScreen().y);
+		Node newNode = new Node(x,y);
+		
+		for (Node node : graph.getNodeList()) {
+			if(isNear(node,newNode))
 			{
-				x = testPoint.get(0);
-				y = testPoint.get(1);
+				x = node.getPointX();
+				y = node.getPointY();
 				
 			}
 		}
-		point.add(x);
-		point.add(y);
-		pointList.add((ArrayList<Integer>) point);
+		try {
+			graph.addNode(new Node(x, y));
+		} catch (AddNodeException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		try {
+			panel.AddPoint(x,y);
+		} catch (AddNodeException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 		
-		panel.AddPoint(x, y);
 		
 		
+	
+
+	}
+
+	private boolean isNear(Node node, Node newNode) {
+		if (Math.abs(node.getPointX().intValue() - newNode.getPointX().intValue()) <= GAP 
+			&& Math.abs(node.getPointY().intValue()-newNode.getPointY().intValue()) <= GAP ){
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		x = BigDecimal.valueOf(e.getXOnScreen()-3-panel.getLocationOnScreen().x);
+		y = BigDecimal.valueOf(e.getYOnScreen()-3-panel.getLocationOnScreen().y);
+		Node newNode = new Node(x,y);
+		
+		for (Node node : graph.getNodeList()) {
+			if(isNear(node,newNode))
+			{
+				x = node.getPointX();
+				y = node.getPointY();
+				
+			}
+		}
+		try {
+			graph.addNode(new Node(x, y));
+		} catch (AddNodeException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		try {
+			panel.AddPoint(x,y);
+		} catch (AddNodeException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 		
 	}
 
-
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 }
--- src/lt/refactory/primsAlgo/gui/GraphMouseMotionListener.java
+++ src/lt/refactory/primsAlgo/gui/GraphMouseMotionListener.java
@@ -2,9 +2,22 @@ package lt.refactory.primsAlgo.gui;
 
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JPanel;
 
 public class GraphMouseMotionListener implements MouseMotionListener {
 
+	JPanel panel;
+	int checkedPointX;
+	int checkedPointY;
+	List<ArrayList<Integer>> pointList = new ArrayList<>();
+	
+	public GraphMouseMotionListener(GraphPanel panel) {
+		// TODO Auto-generated constructor stub
+	}
+	
 	@Override
 	public void mouseDragged(MouseEvent arg0) {
 		// TODO Auto-generated method stub
@@ -15,6 +28,9 @@ public class GraphMouseMotionListener implements MouseMotionListener {
 	public void mouseMoved(MouseEvent arg0) {
 		
 			
+	}
+	public void AddPoint(ArrayList<Integer> point){
+		pointList.add(point);
 	}
 
 }
